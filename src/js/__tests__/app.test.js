f160a44d2529501473f296a409b4f0937af1d0c5
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2022-04-10 16:45:26 +0300

    First commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,14 @@
+import healthLevel from '../app';
+
+const checkObj = [
+  [{ name: 'Маг', health: 90 }, 'healthy'],
+  [{ name: 'Лекарь', health: 45 }, 'wounded'],
+  [{ name: 'Воин', health: 12 }, 'critical'],
+];
+
+const handler = test.each(checkObj);
+
+handler('check healf level for %O', (person, expected) => {
+  const result = healthLevel(person);
+  expected(result).toBe(expected);
+});
